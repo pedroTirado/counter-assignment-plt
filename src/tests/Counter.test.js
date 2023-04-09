3d293175d81f9a99bea019ea1b9a3e03b5d4eb44
pedroTirado <17729184+pedroTirado@users.noreply.github.com>
2023-04-09 18:34:33 -0400

    passed first two tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,34 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import React from 'react'
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
 
-beforeEach(() => {
-  // Render the Counter component here
-})
+// beforeEach(() => {
+//   // Render the Counter component here ---> DEPRECATED DUE TO ESLINT RULE
+// })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counterMessage = screen.getByText('Counter');
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const initCount = screen.getByTestId('count');
+  expect(initCount).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
 });
